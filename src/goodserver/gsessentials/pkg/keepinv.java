e917430747aebc65ad31657014da2f0a43c9a97b
Jackwd40 <Davies.jacksonw@gmail.com>
2020-07-05 03:11:10 -0500

    Started gse

---
+++ src/goodserver/gsessentials/pkg/keepinv.java
@@ -0,0 +1,17 @@
+package goodserver.gsessentials.pkg;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class keepinv implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        
+
+        return true;
+    }
+
+
+}
