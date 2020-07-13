3684c3a7f303d59fe5168d424a40b640088d85bb
Jackwd40 <Davies.jacksonw@gmail.com>
2020-07-13 17:12:37 -0500

    Pre-sara push

--- src/goodserver/gsessentials/pkg/main.java
+++ src/goodserver/gsessentials/pkg/main.java
@@ -37,42 +37,45 @@ public class main extends JavaPlugin {
         }
 
 
+//        getCommand("NAME OF COMMAND").setExecutor(new CLASS NAME);
+
         getCommand("rankhelp").setTabCompleter(new TabCompletion());
         getCommand("rankhelp").setExecutor(new rankhelp());
+        getCommand("ac").setExecutor(new gsac());
 
-//        admin paycheck
-        LocalDateTime now = LocalDateTime.now();
-
-        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
-
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Date date = new Date();
-                String[] list = formatter.format(date).split(":");
-                while (true) {
-                    if (list[0].equals("05") && list[1].equals("30") ){
-                        econ.depositPlayer("magicalsnake19", 50000);
-                        econ.depositPlayer("CannonFodderDos", 50000);
-                        System.out.println("Paid the Admin.");
-                        try {
-                            Thread.sleep(86400000);
-                        } catch (InterruptedException e) {
-                            // do nothing
-                        }
-                    } else {
-                        try {
-                            Thread.sleep(30000);
-                            date = new Date();
-                            list = formatter.format(date).split(":");
-                        } catch (InterruptedException e) {
-                            // do nothing
-                        }
-                    }
-                }
-            }
-        }).start();
+////        admin paycheck
+//        LocalDateTime now = LocalDateTime.now();
+//
+//        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+//
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Date date = new Date();
+//                String[] list = formatter.format(date).split(":");
+//                while (true) {
+//                    if (list[0].equals("05") && list[1].equals("30") ){
+//                        econ.depositPlayer("magicalsnake19", 50000);
+//                        econ.depositPlayer("CannonFodderDos", 50000);
+//                        System.out.println("Paid the Admin.");
+//                        try {
+//                            Thread.sleep(86400000);
+//                        } catch (InterruptedException e) {
+//                            // do nothing
+//                        }
+//                    } else {
+//                        try {
+//                            Thread.sleep(30000);
+//                            date = new Date();
+//                            list = formatter.format(date).split(":");
+//                        } catch (InterruptedException e) {
+//                            // do nothing
+//                        }
+//                    }
+//                }
+//            }
+//        }).start();
     }
 
     //    setup econ as vault recommends
